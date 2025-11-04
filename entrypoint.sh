5626f40672a201194fa684ee95a67e509bebc51d
sabonerune <102559104+sabonerune@users.noreply.github.com>
2025-11-05 06:54:52 +0900

    FIX: 一部の引数の固定化

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 set -eu
 
-args=("$@")
-set_voicevox_dir=
-set_voicelib_dir=
-set_runtime_dir=
-for arg in "$@"; do
-	if [[ "$arg" =~ ^--voicevox_dir(=.*)?$ ]]; then
-		set_voicevox_dir=0
-	elif [[ "$arg" =~ ^--voicelib_dir(=.*)?$ ]]; then
-		set_voicelib_dir=0
-	elif [[ "$arg" =~ ^--runtime_dir(=.*)?$ ]]; then
-		set_runtime_dir=0
-	fi
-done
-
-if [[ -z "$set_voicevox_dir" && -z "$set_voicelib_dir" ]]; then
-	# Set default voicelib directory
-	args+=("--voicelib_dir=/opt/voicevox_core/lib")
-	export VV_MODELS_ROOT_DIR="${VV_MODELS_ROOT_DIR:-/opt/voicevox_vvm/vvms}"
-fi
-
-if [[ -z "$set_voicevox_dir" && -z "$set_runtime_dir" ]]; then
-	# Set default runtime directory
-	args+=("--runtime_dir=/opt/voicevox_onnxruntime/lib")
-fi
-
 # Display README for engine
 cat /opt/voicevox_engine/README.md >&2
 
-exec "${args[@]}"
+exec /opt/voicevox_engine/.venv/bin/python3 /opt/voicevox_engine/run.py "$@"
